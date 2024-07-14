a05550dabe79307d5971ea9949c9d60a7a9d7750
Joao Pedro <joopedrof.silva@gmail.com>
2024-07-14 15:04:25 -0300

    getpinfo user code

---
+++ user/getpinfo.c
@@ -0,0 +1,31 @@
+#include "kernel/types.h"
+#include "kernel/pstat.h"
+#include "kernel/param.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc > 1){
+    fprintf(2, "Usage: getpinfo\n");
+    exit(1);
+  }
+
+  
+  struct pstat *ps;
+  if(getpinfo(ps) < 0)
+  {
+    fprintf(2, "getpinfo failed\n");
+    exit(1);
+  }
+
+  fprintf(2, "PID\tTICKETS\tTICKS\n");
+  for(int i = 0; i < NPROC; i++){
+    if(ps->inuse[i])
+    {
+        fprintf(2, "%d\t%d\t%d\n", ps->pid[i], ps->tickets[i], ps->ticks[i]);
+    }
+  }
+
+  exit(0);
+}
